a6dd88f206ae28c62f6f6e0e395b7ea3c9ca8891
aurapm <aura.pm8@gmail.com>
2022-08-27 18:56:47 -0300

    Entrega 1
    Creación de un login
    Products Autos

---
+++ js/products.js
@@ -0,0 +1,39 @@
+const container = document.getElementById("container");
+let carsArray = []
+const DATA_CARS = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+function showData(dataArray) {
+
+    for(let i = 0; i < dataArray.products.length; i++){
+        let carsArray = dataArray.products[i];
+    
+        container.innerHTML += 
+        `
+            <div class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${carsArray.image}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${carsArray.name} - ${carsArray.currency} ${carsArray.cost}</h4>
+                            <small class="text-muted">${carsArray.soldCount} vendidos </small>
+                        </div>
+                        <p class="mb-1">${carsArray.description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+    }    
+}   
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(DATA_CARS).then(function(resultObj){
+        if (resultObj.status === "ok"){
+           dataArray = resultObj.data
+           showData(dataArray)
+        }
+    })
+})
+
+
